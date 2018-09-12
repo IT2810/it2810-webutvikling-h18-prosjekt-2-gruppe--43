02ca5bd93140c19c25a88e71eaf77057f080ecf0
Magnus Buvarp <magnus.buvarp@wartsila.com>
2018-09-12 13:52:11 +0200

    Added images and sounds

---
+++ prosjekt2/src/data/compile_data.py
@@ -0,0 +1,29 @@
+images = [
+    'assets/images/undraw_blank_canvas_3rbb.svg',
+    'assets/images/undraw_Calculator_0evy.svg',
+    'assets/images/undraw_city_driver_jh2h.svg',
+    'assets/images/undraw_designer_life_w96d.svg',
+    'assets/images/undraw_in_the_pool_c5h0.svg',
+    'assets/images/undraw_on_the_office_fbfs.svg',
+    'assets/images/undraw_personalization_triu.svg',
+    'assets/images/undraw_product_teardown_elol.svg',
+    'assets/images/undraw_science_fqhl.svg',
+    'assets/images/undraw_server_status_5pbv.svg',
+    'assets/images/undraw_track_and_field_33qn.svg',
+    'assets/images/undraw_wireframing_nxyi.svg'
+]
+
+sounds = [
+    'Black_Ant_-_01_-_Fater_Lee.mp3',
+    'BoxCat_Games_-_10_-_Epic_Song.mp3',
+    'Broke_For_Free_-_01_-_Night_Owl.mp3',
+    'Broke_For_Free_-_05_-_Something_Elated.mp3',
+    'Gillicuddy_-_05_-_Springish.mp3',
+    'Jahzzar_-_05_-_Siesta.mp3',
+    'Jason_Shaw_-_RUNNING_WATERS.mp3',
+    'Kai_Engel_-_04_-_Moonlight_Reprise.mp3',
+    'Monk_Turner__Fascinoma_-_01_-_Its_Your_Birthday.mp3',
+    'Podington_Bear_-_Starling.mp3',
+    'The_Kyoto_Connection_-_09_-_Hachiko_The_Faithtful_Dog.mp3',
+    'Tours_-_01_-_Enthusiast.mp3'
+]
