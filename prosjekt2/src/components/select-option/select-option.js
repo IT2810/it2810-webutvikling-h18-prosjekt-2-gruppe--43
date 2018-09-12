b33afee183ca883398af3c9c9c81f778efc152c7
Yngve Molnes <yngve.molnes@gmail.com>
2018-09-12 14:10:08 +0200

    Add initial component structure

---
+++ prosjekt2/src/components/select-option/select-option.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './select-option.css';
+
+class SelectOption extends Component {
+  render() {
+    return (
+      <div className="select-option">
+      </div>
+    );
+  }
+}
+
+export default SelectOption;
