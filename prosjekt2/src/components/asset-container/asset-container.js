daeaf266df8e2003c7c9ebd673f7c0639c64ce80
Yngve Molnes <yngve.molnes@gmail.com>
2018-09-12 14:10:08 +0200

    Add initial component structure

---
+++ prosjekt2/src/components/asset-container/asset-container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './asset-conatiner.css';
+
+class AssetContainer extends Component {
+  render() {
+    return (
+      <div className="asset-conatiner">
+      </div>
+    );
+  }
+}
+
+export default AssetContainer;
