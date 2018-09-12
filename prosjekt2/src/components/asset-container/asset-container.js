215962815a1a3ec88a7bc2f33e3985bca6951e5e
Magnus Buvarp <magnus.buvarp@wartsila.com>
2018-09-12 15:13:22 +0200

    Created basic layout sections

--- prosjekt2/src/components/asset-container/asset-container.js
+++ prosjekt2/src/components/asset-container/asset-container.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import './asset-conatiner.css';
+import './asset-container.css';
 
 class AssetContainer extends Component {
   render() {
     return (
-      <div className="asset-conatiner">
+      <div className="asset-container">
+        <section className="asset"></section>
+        <section className="author-label label"></section>
+        <section className="author info"></section>
+        <section className="source-label label"></section>
+        <section className="source info"></section>
       </div>
     );
   }
