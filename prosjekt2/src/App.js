a0b07d65d60ddb4ef9bb74be6cb48fe5c4fbf8f8
Magnus Buvarp <magnusb.93@gmail.com>
2018-09-21 04:09:57 +0200

    Now works with passed assets as props

--- prosjekt2/src/App.js
+++ prosjekt2/src/App.js
@@ -2,19 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header'
 import AssetContainer from './components/asset-container/asset-container'
+
 const filter = require('./components/select-option/filter')
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      filter: {
+        imageCategory: 1,
+        soundCategory: 1,
+        textCategory: 1
+      },
+      assets: null
+    }
+  }
+
   render() {
-    filter(1, 1, 1).then(res => console.log(res)).catch(err => console.error(err))
+
     return (
       <div className="app">
         <Header/>
         <div className="content">
-          <AssetContainer tabs={['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4', 'Tab 5', 'Tab 6']} />
+          <AssetContainer assets={ this.state.assets }/>
         </div>
       </div>
     );
+  }
+  componentDidMount() {
+    // Get randomly filtered elements on first load
+    filter(this.state.filter.imageCategory, this.state.filter.soundCategory, this.state.filter.textCategory)
+      .then(res => {
+        this.setState({ assets: res })
+      })
+      .catch(err => console.error(err))
+
+    // Remember: set filter initial values to randomized ints
   }
 }
 
--- prosjekt2/src/components/asset-container/asset-container.js
+++ prosjekt2/src/components/asset-container/asset-container.js
@@ -2,40 +2,52 @@ import React, { Component } from 'react';
 import './asset-container.css';
 
 class AssetContainer extends Component {
-
   constructor(props) {
     super(props)
 
     this.state = {
-      tabs: this.computeTabs(props.tabs)
+      currentTab: null
     }
   }
 
-  computeTabs(tb) {
-    return tb.reduce((acc, cur, ind) => {
-      if (ind < 4) acc.tabs.push(cur)
+  computeTabs(assets) {
+    if (!assets) return null
+
+    return assets.reduce((acc, cur, ind) => {
+      if (ind < 4) acc.tabs.push(cur[2].title)
       else acc.extra++
+
       return acc
     }, { tabs: [], extra: 0 })
   }
 
+  selectTab(evt, index) {
+    console.log(index)
+  }
+
   render() {
-    const tabs = this.state.tabs
-    const extra = tabs.extra ? <div className="tab extra">{ tabs.extra } more</div> : null
+    const tabs = this.computeTabs(this.props.assets)
+
+    let tabsElmt
+    if (tabs && tabs.tabs) {
+      tabsElmt = tabs.tabs.map((tab, ind) => {
+        return <div className="tab" key={ ind } onClick={evt => this.selectTab(evt, ind)}>{ tab }</div>
+      })
+    } else {
+      tabsElmt = <div className="tabs-loading"></div>
+    }
+
+    const extra = tabs && tabs.extra ? <div className="tab extra">{ tabs.extra } more</div> : null
 
     return (
       <div className="asset-container">
         <section className="tabs">
-          {
-            tabs.tabs.map((tab, index) => {
-              return <div className="tab" key={ index }>{ tab }</div>
-            })
-          }
-          {
-            extra
-          }
+          { tabsElmt }
+          { extra }
         </section>
-        <section className="asset"></section>
+        <section className="asset">
+
+        </section>
         <section className="author-label label">Utgiver</section>
         <section className="author-info info">Whatever</section>
         <section className="source-label label">Kilde</section>
